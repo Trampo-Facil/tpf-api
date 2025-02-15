78cb211e82381462851a5fc425ac21a0431819a6
Fabrício Lima <105167072+IFafaa@users.noreply.github.com>
2025-02-15 12:23:53 -0300

    chore: removing app-service (#2)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigurationModule, DatabaseModule } from './infra';
 
 @Module({
   imports: [ConfigurationModule.register(), DatabaseModule.register()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
