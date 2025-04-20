30d4071cfbf89b0da6030b3eb7d798ef7919cbdd
Fabricio Lima <fabriciotrab2005@gmail.com>
2025-04-20 16:26:42 -0300

    chore: domain module

---
+++ libs/core/domain/src/lib/entities/user.entity.ts
@@ -0,0 +1,59 @@
+import { Entity, OneToOne, PrimaryKey, Property } from '@mikro-orm/core';
+import { IWorker, Worker } from './worker.entity';
+
+export interface ICreateUserEntityDTO {
+  name: string;
+  password: string;
+  email: string;
+  phone: string;
+}
+export abstract class IUser {
+  abstract id: number;
+  abstract name: string;
+  abstract password: string;
+  abstract email: string;
+  abstract phone: string;
+  abstract worker?: IWorker;
+
+  abstract setWorker(worker: IWorker): void;
+}
+
+@Entity({ tableName: 'user' })
+export class User implements IUser {
+  @PrimaryKey()
+  readonly id!: number;
+
+  @Property()
+  name!: string;
+
+  @Property({ hidden: true })
+  password!: string;
+
+  @Property({ unique: true })
+  email!: string;
+
+  @Property({ unique: true })
+  phone!: string;
+
+  // @OneToOne(() => Client, (client) => client.user, { nullable: true })
+  // client?: Client;
+
+  @OneToOne(() => Worker, { joinColumn: 'worker_id', nullable: true })
+  worker?: IWorker;
+
+  constructor(props: ICreateUserEntityDTO) {
+    const { name, password, email, phone } = props;
+    this.name = name;
+    this.password = password;
+    this.email = email;
+    this.phone = phone;
+  }
+
+  static create(props: ICreateUserEntityDTO): IUser {
+    return new User(props);
+  }
+
+  setWorker(worker: IWorker) {
+    this.worker = worker;
+  }
+}
